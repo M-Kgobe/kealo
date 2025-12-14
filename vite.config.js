171cce0b7af6f1a32daff8f466575697df041277
M-Kgobe <kgobemalesela@gmail.com>
2025-12-14 15:26:06 +0200

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// filepath: /c:/Mali/WEB_ANALYTICS/WEBDEV/Kealo/kealo-hr-website/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/kealo-hr-website/", // Set the base path to match your repository name
-  plugins: [react()],
+	base: "/kealo-hr-website/",
+	plugins: [react()],
 });
