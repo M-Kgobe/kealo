b8ee7f9301c3d7daf423fc1f217244ce46f255b7
M-Kgobe <kgobemalesela@gmail.com>
2025-12-19 06:02:58 +0200

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/kealo-hr-website/",
+	base: "/kealo/",
 	plugins: [react()],
 });
